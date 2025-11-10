b38b81cc3f00f1b49a82be5b3737600750691197
t-mizoguchi <t.mizoguchi.aivan@gmail.com>
2025-11-10 16:44:07 +0900

    add: Employee.java, Department.java DTOの導入
    狙い：保守性、拡張性の向上
    DBControllerクラスにハードコーディングされているコンソール表示をまとめるためにDTOを導入した。

---
+++ refactoring_app_name/src/jp/co/sss/crud/dto/Department.java
@@ -0,0 +1,25 @@
+package jp.co.sss.crud.dto;
+
+public class Department {
+	
+	// 部署ID
+	private int deptId;
+	// 部署名
+	private String deptName;
+	
+	public int getDeptId() {
+		return deptId;
+	}
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
+	public String getDeptName() {
+		return deptName;
+	}
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	
+	
+
+}
---
+++ refactoring_app_name/src/jp/co/sss/crud/dto/Employee.java
@@ -0,0 +1,77 @@
+package jp.co.sss.crud.dto;
+
+import java.util.Date;
+
+import jp.co.sss.crud.util.ConstantMsg;
+
+public class Employee {
+
+	// 社員ID
+	private int empId;
+	// 社員名
+	private String empName;
+	// 性別
+	private int gender;
+	// 生年月日
+	private Date birthday;
+	// 部署ID
+	private Department department;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public int getGender() {
+		return gender;
+	}
+
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	@Override
+	public String toString() {
+		String gender_ja = ConstantMsg.BLANK;
+		if (this.gender == 0) {
+			gender_ja = ConstantMsg.NO_ANSWER;
+		} else if (this.gender == 1) {
+			gender_ja = ConstantMsg.MALE;
+		} else if (this.gender == 2) {
+			gender_ja = ConstantMsg.FEMALE;
+		} else if (this.gender == 9) {
+			gender_ja = ConstantMsg.OTHER;
+		}
+
+		return empId + ConstantMsg.TAB + empName + ConstantMsg.TAB + gender_ja
+				+ ConstantMsg.TAB + birthday + ConstantMsg.TAB + department.getDeptName();
+	}
+
+}
