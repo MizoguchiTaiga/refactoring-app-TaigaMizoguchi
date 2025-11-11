acab14eaf7bfc37ca3d1decebd7ed0c08be523f8
t-mizoguchi <t.mizoguchi.aivan@gmail.com>
2025-11-11 17:57:11 +0900

    fix: MainSystem.java サービスレイヤーの導入とコンソール入出力の統一
    狙い；可読性、保守性の向上
    Serviceクラス導入とコンソール入出力の統一化に伴ってMainにハードコーディングされていた記述を整理した。

--- refactoring_app_name/src/jp/co/sss/crud/main/MainSystem.java
+++ refactoring_app_name/src/jp/co/sss/crud/main/MainSystem.java
@@ -1,13 +1,17 @@
 package jp.co.sss.crud.main;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.sql.SQLException;
 import java.text.ParseException;
 
-import jp.co.sss.crud.db.DBController;
-import jp.co.sss.crud.util.ConstantMsg;
+import jp.co.sss.crud.io.ConsoleWriter;
+import jp.co.sss.crud.io.MenuNoReader;
+import jp.co.sss.crud.service.EmployeeAllFindService;
+import jp.co.sss.crud.service.EmployeeDeleteService;
+import jp.co.sss.crud.service.EmployeeFindByDeptIdService;
+import jp.co.sss.crud.service.EmployeeFindByEmpNameService;
+import jp.co.sss.crud.service.EmployeeRegisterService;
+import jp.co.sss.crud.service.EmployeeUpdateService;
 import jp.co.sss.crud.util.ConstantValue;
 
 /**
@@ -27,89 +31,50 @@ public class MainSystem {
 	 * @throws ParseException 
 	 */
 	public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException, ParseException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int menuNo = ConstantValue.MENU_DEFAULT;
 
 		do {
 			// メニューの表示
-			System.out.println(ConstantMsg.MENU_TITLE);
-			System.out.println(ConstantMsg.MENU_1);
-			System.out.println(ConstantMsg.MENU_2);
-			System.out.println(ConstantMsg.MENU_3);
-			System.out.println(ConstantMsg.MENU_4);
-			System.out.println(ConstantMsg.MENU_5);
-			System.out.println(ConstantMsg.MENU_6);
-			System.out.println(ConstantMsg.MENU_7);
-			System.out.print(ConstantMsg.MSG_ENTER_MENU_NUMBER);
+			ConsoleWriter.showMenu();
 
 			// メニュー番号の入力
-			String menuNoStr = br.readLine();
-			menuNo = Integer.parseInt(menuNoStr);
+			menuNo = MenuNoReader.inputMenuNo();
 
 			// 機能の呼出
 			switch (menuNo) {
 			case ConstantValue.MENU_FIND_ALL_EMP:
 				// 全件表示機能の呼出
-				DBController.findAllEmp();
+				EmployeeAllFindService.execute();
 				break;
 
 			case ConstantValue.MENU_FIND_EMP_BY_NAME:
-				// 社員名検索
-				System.out.print(ConstantMsg.ENTER_EMP_NAME);
-
 				// 検索機能の呼出
-				DBController.findEmpByName();
+				EmployeeFindByEmpNameService.execute();
 				break;
 
 			case ConstantValue.MENU_FIND_EMP_BY_DEPT_ID:
-				// 検索する部署IDを入力
-				System.out.print(ConstantMsg.MSG_ENTER_DEPT_ID);
-				String deptIdForSearch = br.readLine();
-
 				// 検索機能の呼出
-				DBController.findEmpByDeptId(deptIdForSearch);
+				EmployeeFindByDeptIdService.execute();
 				break;
 
 			case ConstantValue.MENU_INSERT_EMP:
-				// 登録する値を入力
-				System.out.print(ConstantMsg.ENTER_EMP_NAME);
-				String empName = br.readLine();
-				System.out.print(ConstantMsg.ENTER_GENDER);
-				String gender = br.readLine();
-				System.out.print(ConstantMsg.ENTER_BIRTHDAY);
-				String birthday = br.readLine();
-				System.out.print(ConstantMsg.ENTER_DEPT_ID);
-				String deptIdForRegist = br.readLine();
-
 				// 登録機能の呼出
-				DBController.insertEmp(empName, gender, birthday, deptIdForRegist);
+				EmployeeRegisterService.execute();
 				break;
 
 			case ConstantValue.MENU_UPDATE_EMP:
-				// 更新する社員IDを入力
-				System.out.print(ConstantMsg.MSG_ENTER_EMP_ID_FOR_UPDATE);
-
-				// 更新する値を入力する
-				String empId = br.readLine();
-				Integer.parseInt(empId);
-
 				// 更新機能の呼出
-				DBController.updateEmp(empId);
-				System.out.println(ConstantMsg.MSG_COMPLETE_UPDATE);
-
+				EmployeeUpdateService.execute();
 				break;
 
 			case ConstantValue.MENU_DELETE_EMP:
-				// 削除する社員IDを入力
-				System.out.print(ConstantMsg.MSG_ENTER_EMP_ID_FOR_DELETE);
-
 				// 削除機能の呼出
-				DBController.deleteEmp();
+				EmployeeDeleteService.execute();
 				break;
 
 			}
 		} while (menuNo != ConstantValue.MENU_END_SYSTEM);
-		System.out.println(ConstantMsg.MSG_END_SYSTEM);
+		ConsoleWriter.showEndSystem();
 	}
 }
